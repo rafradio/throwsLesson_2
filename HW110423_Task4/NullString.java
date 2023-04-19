5142eb6b7e83b638f092fe4e71f15222f2114e95
Rafail Abdyushev <rafaelab@mail.ru>
2023-04-19 21:06:01 +0300

    task 4

---
+++ HW110423_Task4/NullString.java
@@ -0,0 +1,27 @@
+package HW110423_Task4;
+
+import java.util.*;
+
+public class NullString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = null;
+
+        do {
+            System.out.println("Введите пожалуйста строчку:");
+            str = in.nextLine();
+
+            try {
+                if (str.isEmpty()) {
+                    throw new NullPointerException("Пустые строчки вводить нельзя");
+                }
+                else {System.out.println("все ок");}
+
+            }
+            catch(NullPointerException ex) {System.out.println(ex);}
+            
+            
+        }
+        while (str.compareTo("exit") != 0);
+    }
+}
